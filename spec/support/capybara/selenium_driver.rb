18d25cb535e609125c5076c480be15e3196dd7bb
Martin Gruner <mg@zammad.com>
2025-03-16 18:53:02 +0100

    Maintenance: Optimize Chrome settings for CI usage

--- spec/support/capybara/selenium_driver.rb
+++ spec/support/capybara/selenium_driver.rb
@@ -36,7 +36,16 @@ def build_chrome_driver(app, user_agent: nil)
       'profile.default_content_setting_values.notifications' => 1, # ALLOW notifications
     },
     # Disable shared memory usage as it does not really provide a performance gain but cause resource limit issues in CI.
-    args:             %w[--enable-logging --v=1 --disable-dev-shm-usage --disable-search-engine-choice-screen],
+    #   https://peter.sh/experiments/chromium-command-line-switches/
+    args:             %w[
+      --enable-logging
+      --v=1
+      --disable-component-update
+      --disable-dev-shm-usage
+      --disable-features=OptimizationGuideModelDownloading,OptimizationHintsFetching,OptimizationTargetPrediction,OptimizationHints
+      --disable-search-engine-choice-screen
+      --no-first-run
+    ],
     # Disable the "Chrome is being controlled by automated test software." info bar.
     exclude_switches: ['enable-automation'],
   )
